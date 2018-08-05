70fb14c815f1ba66d7be381c48d2d40291386910
Uday Kumar <udaydotkumar@gmail.com>
2018-08-05 16:09:47 -0400

    coding after a while

---
+++ 01_hello_world/program.py
@@ -0,0 +1,9 @@
+print('---------------------------')
+print('           Hello App')
+print('---------------------------')
+print('')
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
