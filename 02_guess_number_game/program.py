4024391f610e723fe7eb3bf2784e01d08cea47fd
Uday Kumar <udaydotkumar@gmail.com>
2018-08-07 23:10:18 -0400

    second program file

--- 02_guess_number_game/program.py
+++ 02_guess_number_game/program.py
@@ -7,10 +7,17 @@ print()
 
 the_number = random.randint(0, 100)
 
-guess_text = input("Guess a number between 0 and 100: ")
+guess = -1
 
-guess = int(guess_text)
 
-print(guess_text, type(guess_text))
+while guess != the_number:
+    guess_text = input("Guess a number between 0 and 100: ")
+    guess = int(guess_text)
+    if guess < the_number:
+        print('too low')
+    elif guess > the_number:
+        print('too high')
+    else:
+        print('you win')
 
-print(guess, type(guess))
+print('done')
