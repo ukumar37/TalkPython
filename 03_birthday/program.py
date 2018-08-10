d1a5f4596f7512a30d6932a8d40539e9647d774d
Uday Kumar <udaydotkumar@gmail.com>
2018-08-09 23:25:14 -0400

    starting to get a rythm

---
+++ 03_birthday/program.py
@@ -0,0 +1,22 @@
+def print_header():
+    pass
+
+
+def get_birthday_from_user():
+    pass
+
+
+def compute_days_between_dates():
+    pass
+
+
+def print_birthday_information():
+    pass
+
+
+def main():
+    print_header()
+    bday = get_birthday_from_user()
+    now = None
+    number_of_days = compute_days_between_dates(bday, now)
+    print_header(number_of_days)
