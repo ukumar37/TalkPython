5455f40bfdb3b118334f4cc423a7a2ec14973c5c
Uday Kumar <udaydotkumar@gmail.com>
2018-08-10 23:52:02 -0400

    finally done

--- 03_birthday/program.py
+++ 03_birthday/program.py
@@ -17,20 +17,26 @@ def get_birthday_from_user():
     return birthday
 
 
-def compute_days_between_dates():
-    pass
+def compute_days_between_dates(original_date, target_date):
+    this_year = datetime.date(target_date.year, original_date.month, original_date.day)
+    dt = this_year - target_date
+    return dt.days
 
 
-def print_birthday_information():
-    pass
+def print_birthday_information(days):
+    if days < 0:
+        print("You had your birthday {} days ago this year.".format(-days))
+    elif days > 0:
+        print("Your birthday is in {} days.".format(days))
+    else:
+        print("Happy Birthday!!!")
 
 
 def main():
     print_header()
     bday = get_birthday_from_user()
-    print(bday)
-    now = None
-    number_of_days = compute_days_between_dates(bday, now)
-    print_header(number_of_days)
+    today = datetime.date.today()
+    number_of_days = compute_days_between_dates(bday, today)
+    print_birthday_information(number_of_days)
 
 main()
