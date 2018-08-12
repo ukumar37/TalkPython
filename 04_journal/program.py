8e3c68d2676c3998d6fad8cf9080b954c0df5661
Uday Kumar <udaydotkumar@gmail.com>
2018-08-12 14:50:37 -0400

    added more to the daily journal app

--- 04_journal/program.py
+++ 04_journal/program.py
@@ -4,10 +4,30 @@ def main():
 
 
 def print_header():
-    pass
+    print('-------------------------')
+    print('      Journal App')
+    print('-------------------------')
+    print()
 
 
 def run_event_loop():
-    pass
+
+    print('What do you want to do with your journal?')
+
+    cmd = None
+
+    while cmd != 'X':
+
+        cmd = input('[L]ist entries, [A]dd an entry, E[X]it: ')
+
+        if cmd.lower().strip() == 'l':
+            print('L')
+        elif cmd.lower().strip() == 'a':
+            print('A')
+        elif cmd.lower().strip() != 'x':
+            print("Sorry. We don't understand '{}'.".format(cmd))
+
+    print('Done. Goodbye!')
 
 main()
+
