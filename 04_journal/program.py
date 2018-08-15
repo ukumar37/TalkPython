cb668b938de3b2a68dea8f427d8d1e9c0a925351
Uday Kumar <udaydotkumar@gmail.com>
2018-08-14 22:09:50 -0400

    printing the basic version the daily journal app

--- 04_journal/program.py
+++ 04_journal/program.py
@@ -11,10 +11,10 @@ def print_header():
 
 
 def run_event_loop():
-
     print('What do you want to do with your journal?')
 
     cmd = None
+    journal_data = []  # list()
 
     while cmd != 'x':
 
@@ -22,21 +22,25 @@ def run_event_loop():
         cmd = cmd.lower().strip()
 
         if cmd == 'l':
-            list_entries()
+            list_entries(journal_data)
         elif cmd == 'a':
-            add_entry()
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry. We don't understand '{}'.".format(cmd))
 
     print('Done. Goodbye!')
 
 
-def list_entries():
-    print('Listing ...')
+def list_entries(data):
+    print("Your journal entries are:")
+    entries = reversed(data) # reverse the list order
+    for idx, entry in enumerate(entries): # enumerated command adds indexes to the list, resulting in something calles "tuples"
+        print('* ({}) {}'.format(idx + 1, entry))
 
 
-def add_entry():
-    print('Adding ...')
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    data.append(text)
+
 
 main()
-
