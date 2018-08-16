238ecdbd2be522e786877b2fe4af9eaec6b0714a
Uday Kumar <udaydotkumar@gmail.com>
2018-08-15 22:55:46 -0400

    add another program to the app and imported it in order to use its methods

---
+++ 04_journal/journal.py
@@ -0,0 +1,11 @@
+def load(name):  # load journal
+    # todo: populate from file if it exists
+    return []
+
+
+def save(name, journal_data):  # save journal along with the data
+    pass
+
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
