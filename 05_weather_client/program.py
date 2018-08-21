b46eec10d0975e98ea1bb357ec53dacb21249903
Uday Kumar <udaydotkumar@gmail.com>
2018-08-20 21:25:53 -0400

    starting on the weather app

--- 05_weather_client/program.py
+++ 05_weather_client/program.py
@@ -2,8 +2,9 @@ def main():
     # print the header
     print_the_header()
     # get the zip code
-    get_zip_code()
+    code = get_zip_code()
     # get the html from web
+    get_html_from_web(code)
     # parse the html
     # display the forecast
 
@@ -18,6 +19,12 @@ def print_the_header():
 def get_zip_code():
     zip_code = input('What is your zip code? ')
     print(zip_code)
+    return zip_code
+
+
+def get_html_from_web(zip_code):
+    url = 'https://www.wunderground.com/weather/us/va/ashburn/{}'.format(zip_code)
+    print('url: {}'.format(url))
 
 
 if __name__ == '__main__':  # always use this convention when calling main() method
