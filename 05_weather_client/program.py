8a0b0696e2e93af76f40977a00cca4f1f0a74f6e
Uday Kumar <udaydotkumar@gmail.com>
2018-08-22 16:02:19 -0400

    building upon the weather app

--- 05_weather_client/program.py
+++ 05_weather_client/program.py
@@ -1,11 +1,15 @@
+import requests
+import bs4
+
 def main():
-    # print the header
     print_the_header()
-    # get the zip code
+
     code = get_zip_code()
-    # get the html from web
-    get_html_from_web(code)
-    # parse the html
+
+    html = get_html_from_web(code)
+
+    get_weather_from_html(html)
+
     # display the forecast
 
 
@@ -18,13 +22,23 @@ def print_the_header():
 
 def get_zip_code():
     zip_code = input('What is your zip code? ')
-    print(zip_code)
+    # print(zip_code)
     return zip_code
 
 
 def get_html_from_web(zip_code):
     url = 'https://www.wunderground.com/weather/us/va/ashburn/{}'.format(zip_code)
-    print('url: {}'.format(url))
+    # print('url: {}'.format(url))
+    response = requests.get(url)
+    # print(response.status_code)
+    # print(response.text[0:250])
+
+    return response.text
+
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html)
+    print(soup)
 
 
 if __name__ == '__main__':  # always use this convention when calling main() method
