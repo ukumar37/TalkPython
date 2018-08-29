9f8b566dca1ae947a8b2242d4f433db8040fbae9
Uday Kumar <udaydotkumar@gmail.com>
2018-08-28 22:42:34 -0400

    adding to the lolcat factory program

--- 06_lolcat_factory/cat_service.py
+++ 06_lolcat_factory/cat_service.py
@@ -7,8 +7,10 @@ import requests
 def get_cat(folder, name):
     url = 'http://consuming-python-services-api.azurewebsites.net/cats/random'
     data = get_data_from_url(url)
+    pass
 
 
 def get_data_from_url(url):
     response = requests.get(url)
     return response.raw
+    pass
