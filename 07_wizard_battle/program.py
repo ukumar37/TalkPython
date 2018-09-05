2c949b18198e054712ecb3ec6d83ef684f7137b8
Uday Kumar <udaydotkumar@gmail.com>
2018-09-05 19:59:50 -0400

    adding method to classes Creature and Wizard to the wizard battle

--- 07_wizard_battle/program.py
+++ 07_wizard_battle/program.py
@@ -1,4 +1,6 @@
 # import actors
+import random
+
 from actors import Wizard, Creature
 
 def main():
@@ -15,7 +17,7 @@ def print_header():
 
 def game_loop():
 
-    creatures = [
+    creatures = [  # Instantiate our creatures
         Creature('Toad', 1),
         Creature('Tiger', 12),
         Creature('Bat', 3),
@@ -23,15 +25,21 @@ def game_loop():
         Creature('Evil Wizard', 1000)
     ]
 
-    print(creatures)
+    # print(creatures)
 
-    hero = Wizard('Gandolf', 75)
+    hero = Wizard('Gandolf', 75)  # Instantiate our hero
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from a dark and foggy forest ...'
+              .format(active_creature.name, active_creature.level))
+        print('')
+
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
         if cmd == 'a':
-            print('attack')
+            hero.attack(active_creature)
+            # print('attack')
         elif cmd == 'r':
             print('runaway')
         elif cmd == 'l':
