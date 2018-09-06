4ff3454c915b0e6dcb8df9a00cb1b0bb0cbaafbd
Uday Kumar <udaydotkumar@gmail.com>
2018-09-05 20:09:02 -0400

    adding method to classes Creature and Wizard to the wizard battle

--- 07_wizard_battle/actors.py
+++ 07_wizard_battle/actors.py
@@ -11,6 +11,17 @@ class Wizard:
         print("The wizard {} attacks {}".format(self.name, creature.name))
 
         my_roll = random.randint(1, 12) * self.level
+        creature_roll = random.randint(1, 12) * creature.level
+
+        print("You roll {} ...".format(my_roll))
+        print("{} rolls {} ...".format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print("The wizard has handily triumphed over {}".format(creature.name))
+            return True
+        else:
+            print("The wizard has been DEFEATED!!!")
+            return False
 
 
 
