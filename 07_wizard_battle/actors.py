3142228c10effabcf34b218512dcd0328a151177
Uday Kumar <udaydotkumar@gmail.com>
2018-09-09 13:37:29 -0400

    more code to wizard app

--- 07_wizard_battle/actors.py
+++ 07_wizard_battle/actors.py
@@ -1,17 +1,30 @@
 import random
 
 
-class Wizard:
-    def __init__(self, name, level):  # initialize the class
-        self.name = name  # once instantiated, give the object a name
-        self.level = level  # once instantiated, give the object a level
+class Creature:
+    def __init__(self, name, the_level):
+        self.name = name
+        self.level = the_level
+
+    def __repr__(self):  # output the value of an instance of an object by using a print statement
+        return "Creature {} of level {}".format(
+            self.name, self.level
+        )
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Wizard(Creature):
+    # def __init__(self, name, level):  # initialize the class
+    #    self.name = name  # once instantiated, give the object a name
+    #   self.level = level  # once instantiated, give the object a level
 
 
     def attack(self, creature):
         print("The wizard {} attacks {}".format(self.name, creature.name))
 
-        my_roll = random.randint(1, 12) * self.level
-        # creature_roll = random.randint(1, 12) * creature.level
+        my_roll = self.get_defensive_roll()
         creature_roll = creature.get_defensive_roll()
 
         print("You roll {} ...".format(my_roll))
@@ -25,16 +38,18 @@ class Wizard:
             return False
 
 
-
-class Creature:
-    def __init__(self, name, the_level):
-        self.name = name
-        self.level = the_level
-
-    def __repr__(self):  # output the value of an instance of an object by using a print statement
-        return "Creature {} of level {}".format(
-            self.name, self.level
-        )
-
+class SmallAnimal(Creature):
     def get_defensive_roll(self):
-        return random.randint(1, 12) * self.level
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
+        self.breaths_fire = breaths_fire
+        self.scaliness = scaliness
+
+        def get_defensive_roll(self):
+            base_roll = super().get_defensive_roll()
+        
