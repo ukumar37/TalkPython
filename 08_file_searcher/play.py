c139df8f931466c8a4916fa8ee624ab95de0366e
Uday Kumar <udaydotkumar@gmail.com>
2018-09-12 22:28:04 -0400

    initial skeleton for the file searcher app

---
+++ 08_file_searcher/play.py
@@ -0,0 +1,22 @@
+def main():
+    pass
+
+
+def print_header():
+    pass
+
+
+def get_folder_from_user():
+    pass
+
+
+def get_search_text_from_user():
+    pass
+
+
+def search_file():
+    pass
+
+
+if __name__ == '__main__':
+    main()
