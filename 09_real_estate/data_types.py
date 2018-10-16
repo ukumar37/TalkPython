3f127aabe267d5f656d5c77f343059dfe6ff3bb7
Uday Kumar <udaydotkumar@gmail.com>
2018-10-15 20:59:12 -0400

    populating the Purchases dictionary object

--- 09_real_estate/data_types.py
+++ 09_real_estate/data_types.py
@@ -1,23 +1,23 @@
-
 class Purchase:
     def __init__(
-            self, street, city, zipcode, state, beds,
-            baths, sq__ft, home_type, sale_date, prices,
+            self, street, city, zip, state, beds,
+            baths, sq__ft, home_type, sale_date, price,
             latitude, longitude):
         self.longitude = longitude
         self.latitude = latitude
         self.price = price
         self.sale_date = sale_date
-        self.home_tyoe = home_type
+        self.home_type = home_type
         self.sq__ft = sq__ft
         self.baths = baths
         self.beds = beds
         self.state = state
-        self.zip_code = zip_code
+        self.zip = zip
         self.city = city
         self.street = street
 
-    def create_from_dict(self, lookup)
+    @staticmethod
+    def create_from_dict(lookup):
         return Purchase(
             lookup['street'],
             lookup['city'],
