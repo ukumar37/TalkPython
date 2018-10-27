ad05163dda043b03a84f8fee291e27fe2c706c92
Uday Kumar <udaydotkumar@gmail.com>
2018-10-27 16:28:51 -0400

    searching for hit movie but now with a stand along service/package

---
+++ 10_movie_search/movie_svc.py
@@ -0,0 +1,24 @@
+import requests
+import collections
+
+MovieResult = collections.namedtuple(
+    'MovieResult',
+    'imdb_code, title, duration, director, year, rating, imdb_score, keywords, genres'
+)
+
+
+def find_movies(search_text):
+    url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search_text)
+
+    resp = requests.get(url)  # execute the api and get the movie data
+    resp.raise_for_status()  # check for status code
+
+    movie_data = resp.json()  # get the movie data in json format, this will return a dictionary object
+    movies_list = movie_data.get('hits')  # get the hit movies only, this will return a list object
+
+    movies = [
+        MovieResult(**md)
+        for md in movies_list
+    ]
+
+    return(movies)
---
+++ 10_movie_search/play.py
@@ -0,0 +1,35 @@
+import movie_svc
+
+
+def main():
+    print_header()
+    search_event_loop()
+
+
+def print_header():
+    print("-----------------------------------")
+    print("---------Movie Service App---------")
+    print("-----------------------------------")
+
+
+def search_event_loop():
+    search = 'ONCE_THROUGH_LOOP'
+
+    while search != 'x':
+        search = input('Movie search text (x to exit): ')
+        if search != 'x':
+            results = movie_svc.find_movies(search)
+            print("Found {} results.".format(len(results)))
+            for r in results:
+                print('{} -- {}'.format(
+                    r.year, r.title
+                ))
+            print()
+
+    print("Exiting ...")
+
+
+if __name__ == '__main__':
+    main()
+
+
