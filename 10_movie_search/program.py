912c95a5f5eaf9eaf69a3ef330edb85d9611290a
Uday Kumar <udaydotkumar@gmail.com>
2018-10-27 15:37:50 -0400

    searching for hit movie by keyword using different technical approaches

--- 10_movie_search/program.py
+++ 10_movie_search/program.py
@@ -3,7 +3,7 @@ import collections
 
 MovieResult = collections.namedtuple(
     'MovieResult',
-    'imdb_code, title, duration, director, year, rating, imdb_score, keywords, genre'
+    'imdb_code, title, duration, director, year, rating, imdb_score, keywords, genres'
 )
 
 search = input("What movie do you want to search for?")  # key word in movie title
@@ -15,20 +15,36 @@ resp.raise_for_status()  # check for status code
 movie_data = resp.json()  # get the movie data in json format, this will return a dictionary object
 movies_list = movie_data.get('hits')  # get the hit movies only, this will return a list object
 
-movies = []  # start with an empty list of movies
-for md in movies_list:  # loop movies_list and populate the MovieResult tuple defined earlier
-    m = MovieResult(
-        imdb_code=md.get('imdb_code'),
-        title=md.get('title'),
-        duration=md.get('duration'),
-        director=md.get('director'),
-        year=md.get('year', 0),
-        rating=md.get('rating', 0),
-        imdb_score=md.get('imdb_score', 0.0),
-        keywords=md.get('keywords'),
-        genre=md.get('genre')
-    )
-    movies.append(m)
+# 1st approach using dictionary:
+# movies = []  # start with an empty list of movies
+# for md in movies_list:  # loop movies_list and populate the MovieResult tuple defined earlier
+#     m = MovieResult(
+#         imdb_code=md.get('imdb_code'),
+#         title=md.get('title'),
+#         duration=md.get('duration'),
+#         director=md.get('director'),
+#         year=md.get('year', 0),
+#         rating=md.get('rating', 0),
+#         imdb_score=md.get('imdb_score', 0.0),
+#         keywords=md.get('keywords'),
+#         genres=md.get('genres')
+#     )
+#     movies.append(m)
+
+
+# 2nd approach using kwargs:
+# movies = []  # start with an empty list of movies
+# for md in movies_list:  # loop movies_list and populate the MovieResult tuple defined earlier
+#     m = MovieResult(**md)  # using the kwargs "key word arguments" approach, as long as 1-1 matching
+#     movies.append(m)
+
+
+# 3rd approach using list comprehension and kwargs:
+movies = [
+    MovieResult(**md)
+    for md in movies_list
+]
+
 
 print("Found {} movies for search '{}'. Here is the list:".format(len(movies), search))
 for m in movies:
